48db366fea8fad11dce5746f8bf0b80e92571998
Simolater <s.giesse@gmx.net>
2021-03-06 16:42:08 +0100

    Basic project structure

---
+++ HelloTriangleApplication.cpp
@@ -0,0 +1,25 @@
+#pragma once
+
+class HelloTriangleApplication {
+
+public:
+	void run() {
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
+	
+private:
+
+	void initVulkan() {
+
+	}
+
+	void mainLoop() {
+
+	}
+
+	void cleanup() {
+
+	}
+};
