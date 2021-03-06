f5301659f002118ff16e6117cda4678c632e33b0
Simolater <s.giesse@gmx.net>
2021-03-06 16:53:53 +0100

    First Window

--- HelloTriangleApplication.cpp
+++ HelloTriangleApplication.cpp
@@ -1,9 +1,15 @@
 #pragma once
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
 
 class HelloTriangleApplication {
 
 public:
 	void run() {
+		initWindow();
 		initVulkan();
 		mainLoop();
 		cleanup();
@@ -11,15 +17,30 @@ public:
 	
 private:
 
+	GLFWwindow* window;
+
+	void initWindow() {
+		glfwInit();
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+		window = glfwCreateWindow(WIDTH, HEIGHT, "TestApplication", nullptr, nullptr);
+	}
+
 	void initVulkan() {
 
 	}
 
 	void mainLoop() {
-
+		while (!glfwWindowShouldClose(window)) {
+			glfwPollEvents();
+		}
 	}
 
 	void cleanup() {
+		glfwDestroyWindow(window);
 
+		glfwTerminate();
 	}
 };
